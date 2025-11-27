eeeadd9a41a43d75de007ea04d48037f8a720eac
dirhapauki0129 <dirhapauki0129@users.noreply.github.com>
2025-11-27 20:52:52 +0800

    Initial commit

---
+++ common.tsx
@@ -0,0 +1,3 @@
+import { useState, useEffect } from 'react';
+
+export default function product_customer() { return <div>Component</div>; }
---
+++ frontend/web/src/adapter-subscriptions-admin.tsx
@@ -0,0 +1,3 @@
+import { ethers } from 'ethers';
+
+export default function order() { return <div>Component</div>; }
